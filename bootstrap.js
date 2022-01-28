d79d32b5d061e9803659cf6089f02fbbc89e027e
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2022-01-28 09:32:24 -0500

    Add files via upload

--- bootstrap.js
+++ bootstrap.js
@@ -1,26 +1,36 @@
-$(document).ready(function(){
-// Activate Carousel
-$("#myCarousel").carousel();
-
-// Enable Carousel Indicators
-$(".item1").click(function(){
-  $("#myCarousel").carousel(0);
-});
-$(".item2").click(function(){
-  $("#myCarousel").carousel(1);
-});
-$(".item3").click(function(){
-  $("#myCarousel").carousel(2);
-});
-$(".item4").click(function(){
-  $("#myCarousel").carousel(3);
-});
-
-// Enable Carousel Controls
-$(".left").click(function(){
-  $("#myCarousel").carousel("prev");
-});
-$(".right").click(function(){
-  $("#myCarousel").carousel("next");
-});
-});
+//delete the default name field but not the user input
+	function clearName() {
+		if (document.getElementById('nameField').value == 'Example: John Doe'){
+			document.getElementById('nameField').value = '';
+		}
+	}
+	//bring back the default text if name field is empty
+		function resetName() {
+			if (document.getElementById('nameField').value == ''){
+				document.getElementById('nameField').value = 'Example: John Doe';
+			}
+		}
+	//delete the default phone field but not the user input
+	function clearPhone() {
+		if (document.getElementById('phoneField').value == 'Example: 212-123-1234'){
+			document.getElementById('phoneField').value = '';
+		}
+	}
+	//bring back the default text if phone field is empty
+		function resetPhone() {
+			if (document.getElementById('phoneField').value == ''){
+				document.getElementById('phoneField').value = 'Example: 212-123-1234';
+			}
+		}
+		//delete the default email field but not the user input
+		function clearEmail() {
+			if (document.getElementById('emailField').value == 'Example: johndoe123@gmail.com'){
+				document.getElementById('emailField').value = '';
+			}
+		}
+	//bring back the default text if email field is empty
+		function resetEmail() {
+			if (document.getElementById('emailField').value == ''){
+				document.getElementById('emailField').value = 'Example: johndoe123@gmail.com';
+			}
+		}
