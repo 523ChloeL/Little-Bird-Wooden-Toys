923aea1a52778ba9a4ea4dd4935239698f60517c
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2022-02-10 11:03:04 -0500

    Add files via upload

--- bootstrap.js
+++ bootstrap.js
@@ -1,13 +1,13 @@
 //delete the default name field but not the user input
 function clearName() {
-  if (document.getElementById('nameField').value == 'Example: John Doe') {
+  if (document.getElementById('nameField').value == 'Example: Tony Stark') {
     document.getElementById('nameField').value = '';
   }
 }
 //bring back the default text if name field is empty
 function resetName() {
   if (document.getElementById('nameField').value == '') {
-    document.getElementById('nameField').value = 'Example: John Doe';
+    document.getElementById('nameField').value = 'Example: Tony Stark';
   }
 }
 //delete the default address field but not the user input
@@ -24,14 +24,14 @@ function resetAddress() {
 }
 //delete the default email field but not the user input
 function clearEmail() {
-  if (document.getElementById('emailField').value == 'Example: johndoe123@gmail.com') {
+  if (document.getElementById('emailField').value == 'Example: tonystark3000@starkindustries.com') {
     document.getElementById('emailField').value = '';
   }
 }
 //bring back the default text if email field is empty
 function resetEmail() {
   if (document.getElementById('emailField').value == '') {
-    document.getElementById('emailField').value = 'Example: johndoe123@gmail.com';
+    document.getElementById('emailField').value = 'Example: tonystark3000@starkindustries.com';
   }
 }
 
